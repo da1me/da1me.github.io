e4c6c24353e1dda1749ab49d0cd6b861c1bd0868
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-02-08 15:15:42 +0100

    Prima riscrittura

--- scripts/getHymns.py
+++ scripts/getHymns.py
@@ -1,124 +1,105 @@
+""" Script for scraping hymns """
+
+import re
+from pathlib import Path
+import json
 import requests
 from bs4 import BeautifulSoup
 import nltk as k
-import os
 
-base_url = 'https://nossairmandade.com'
-hymn_path = '/hinarios/individual'  # Add other paths if needed
-hymn_dir = 'hinos'  # folder were text files will be written
-response = requests.get(base_url + hymn_path)
+BASE_URL = 'https://nossairmandade.com'
+HYMN_PATHS = ['/hinarios/individual']
+HYMN_DIR = 'hinos'
 
-soup = BeautifulSoup(response.content, 'html.parser')
-donos2 = soup.find_all('div', 'col-md-7')  # parece ter 1 a mais
+def check_name(name_to_check, list_of_names):
+    """ Check whether there's an omonym, and if so append a counter to the original name """
+    if name_to_check not in list_of_names:
+        return name_to_check
+    counter = 1
+    if len(name_to_check.split('_')) > 1:
+        counter = int(name_to_check.split('_')[1]) + 1
+    return check_name(f"{name_to_check.split('_')[0]}_{counter}", list_of_names)
 
-donos = {}  # hymn "owner", the person who "received" it
-achados = []  # found hymnals
-for num, dono in enumerate(donos2):  # finding owners and her hymnals
-    try:
-        nome = dono.find_all('a')[0].get_text()
-        if nome == 'Jump to list of individuals':
-            continue
-        # TODO: check that dono is unique in donos2
-        if nome in donos:
-            raise Exception('FIXME: A hymnal owner is being overwritten')
-        donos[nome] = {}
 
-        a_ = dono.find_all('h4')
-        for i in a_:
-            a = i.a
-            link = a['href']
-            hinario = a.get_text().strip()
-            print(nome, hinario, link)
-            achados.append((nome, hinario, link))  # useful for debugging / developing
-            donos[nome][hinario] = { 'link': link, 'hymns': [] }  # useful for reaching final text files
-    except:
-        print('something went bad with donos2 item in index:', num)
+for path in HYMN_PATHS:
+    response = requests.get(BASE_URL + path, timeout=10)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# check donos dictionary is as expected
-for dono in donos:
-    for hinario_name in donos[dono]:
-        hinario = donos[dono][hinario_name]
-        link = hinario['link']
-        r = requests.get(link)
-        s = BeautifulSoup(r.content, 'html.parser')
+    # Elenca tutte le persone
+    people_raw = soup.select('.col-md-7:not(.col-sm-6)')
+    catalog = {}
+    for person in people_raw:
+        name = check_name(person.find_all('a')[0].get_text(), catalog)
+        catalog[name] = {}
 
-        hinos = s.find_all('div', 'hymn-list-name')  # parece ter 1 a mais
-        hinos_ = []
-        HINOS = {}
-        count = 0
-        for h in hinos[:1]:
-            nome = h.get_text().strip()
-            link = h.a['href']
+        # Elenca tutti gli hinario per la persona corrente
+        hinarios = person.select('h4 a')
+        for hinario in hinarios:
+            title = hinario.get_text().strip()
+            link = hinario.attrs['href']
+            catalog[name][title] = {'link': link, 'hymns': [] }
+            print(f"{name}, '{title}': {link}")
 
-            hinos_.append((nome, link))
+    # Crea la cartella per salvare i file
+    Path(f"./{HYMN_DIR}").mkdir(parents=True, exist_ok=True)
 
-            # finally opening an hymn:
-            r2 = requests.get(link)
-            s2 = BeautifulSoup(r2.content, 'html.parser')
-            title = s2.find_all('div', 'hymn-title')[0].get_text().strip()
+    # Vaglia ogni persona
+    for person, hinarios in catalog.items():
+        print(f"\n{person.upper()}")
 
-            # maybe assert(title == nome) ??
-            # ===> title parece mais limpo que nome
-            estrofes = s2.find_all('div', 'hymnstanza')
-            HINO = dict(tokens=[], versos=[], title=title)
-            for e in estrofes:
-                repeticoes = e.find_all('div', 'hymn-words')
-                for r in repeticoes:
-                    versos = r.get_text().strip().split('<br>')
-                    HINO['versos'].extend(versos)
-                    for verso in versos:
-                        tokens = k.word_tokenize(verso)
-                        HINO['tokens'].extend(tokens)
-            HINOS[h] = HINO
-            hinario['hymns'].append(HINO)
-            print(count)
-            count += 1
+        # Vaglia ogni hinario della persona corrente
+        for hinario in hinarios:
+            HINARIO = {
+                'title': hinario,
+                'received_by': person,
+                'hinos': []
+                }
+            print(f"+ {hinario}:")
+            link = hinarios[hinario]['link']
+            req = requests.get(link, timeout=10)
+            soup = BeautifulSoup(req.content, 'html.parser')
 
-# qua manca:
-# capire se donos dict finisce con una buona struttura di dato
-# scrivere tutto come un solo file JSON, dopo apriamo questo JSON
-# sia in JS per la webpage, che in Python per analisi numeriche e locali
+            # Ottieni gli hino contenuti nell'hinario corrente
+            hinos_raw = soup.select('.hymn-list-name a')
+            index = 0
+            for hino in hinos_raw:
+                index += 1
+                hino_link = hino.attrs['href']
 
-# se possibile:
-# pulire lo script, mi sembra che ci sono degli oggetti non necessari.
-# scrivere tutto in inglese o italiano o portoghese. Preferisco l'italiano
-# capire se possiamo rimouvere lo script di sotto:
+                req = requests.get(hino_link, timeout=10)
+                soup = BeautifulSoup(req.content, 'html.parser')
 
-######## cancellare?
-# achados_ = [i for i in achados if i[0] != 'Jump to list of individuals']
-# achados_.pop(3)  # o mestre diz, é uma gravação
-# 
-# hinarios = {}
-# for a in achados_[:10]:
-#     r = requests.get(a[2])
-#     s = BeautifulSoup(r.content, 'html.parser')
-#     hinos = s.find_all('div', 'hymn-list-name')  # parece ter 1 a mais
-#     hinos_ = []
-#     HINOS = {}
-#     count = 0
-#     for h in hinos[:1]:
-#         nome = h.get_text().strip()
-#         link = h.a['href']
-#         hinos_.append((nome, link))
-#         r2 = requests.get(link)
-#         s2 = BeautifulSoup(r2.content, 'html.parser')
-#         title = s2.find_all('div', 'hymn-title')[0].get_text().strip()
-#         # assert(title == nome)
-#         # ===> title mais limpo que nome
-#         estrofes = s2.find_all('div', 'hymnstanza')
-#         HINO = dict(tokens=[], versos=[])
-#         for e in estrofes:
-#             repeticoes = e.find_all('div', 'hymn-words')
-#             for r in repeticoes:
-#                 versos = r.get_text().strip().split('<br>')
-#                 HINO['versos'].extend(versos)
-#                 for verso in versos:
-#                     tokens = k.word_tokenize(verso)
-#                     HINO['tokens'].extend(tokens)
-#         HINOS[h] = HINO
-#         print(count)
-#         count += 1
-#     hinarios[a] = HINOS
-# 
-# # qua manca:
-######
+                title = soup.select('.hymn-title h5')[0].get_text().strip()
+                HINO = {
+                    'title': title,
+                    'index': index,
+                    'tokens': [],
+                    'verses': []
+                    }
+                print(f"  - {title}")
+
+                # Analizza le singole strofe
+                stanzas_raw = soup.select('.hymnstanza:has(.hymn-words)')
+                for stanza in stanzas_raw:
+                    repetitions = stanza.select('.hymn-words')
+                    for repetition in repetitions:
+                        verses = re.sub(' +', ' ', repetition.get_text().strip()).split('<br>')
+                        HINO['verses'].extend(verses)
+                        for verse in verses:
+                            tokens = k.word_tokenize(verse)
+                            HINO['tokens'].extend(tokens)
+                
+                # Aggiungi l'hino all'hinario corrente
+                HINARIO['hinos'].append(HINO)
+
+                # Scrivi hino
+                hino_json = json.dumps(HINO, indent=4)
+                filename = f"./{HYMN_DIR}/{person} - {hinario} - {index}. {title}.json"
+                with open(filename, 'w', encoding='utf-8') as outfile:
+                    outfile.write(hino_json)
+            
+            # Scrivi hinario
+            hinario_json = json.dumps(HINARIO, indent=4)
+            filename = f"./{HYMN_DIR}/{person} - {hinario}.json"
+            with open(filename, 'w', encoding='utf-8') as outfile:
+                outfile.write(hinario_json)
